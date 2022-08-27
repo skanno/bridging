1d434411a5288bb03b67e5628027f6c0176090ed
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-08-27 18:47:40 +0900

    初回のコミット

---
+++ server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import Url from 'url-parse';
+import wget from 'wget-improved';
+
+var app = express();
+
+app.get('/', (request, response) => {
+  if (request.query.url === undefined || request.query.url === '') {
+    response.status(400).send('パラメータが異常です。urlパラメータが存在しません。');
+
+    return;
+  }
+
+  let url = new Url(request.query.url);
+  const options = {
+      protocol: url.protocol,
+      host: url.host,
+      path: url.pathname,
+      method: 'GET'
+  };
+
+  let req = wget.request(options, function(res) {
+    let content = '';
+    if (res.statusCode === 200) {
+      res.on('error', function(err) {
+        response.status(400).send('存在しないURLです。');
+      });
+      res.on('data', function(chunk) {
+        content += chunk;
+      });
+      res.on('end', function() {
+        response.status(200).send(content);
+      });
+    } else {
+      response.status(500);
+    }
+  });
+
+  req.end();
+  req.on('error', function(err) {
+    response.status(500);
+  });
+});
+
+app.listen(PORT);
